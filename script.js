25e4a56ee6b60208ed64273dc92b5f334605b300
adi_neo <aditzaabc@gmail.com>
2023-03-14 08:18:59 +0200

    final version s5

--- script.js
+++ script.js
@@ -2,15 +2,19 @@
 // compara alegrile si afiseaza castigatorul
 //radio_checked
 //computer_choice
-function the_choices() {
-    //alegerea playerului, vede care buton e apasat si afiseaza textul butonului
+
+
+//sunt variantele de combinatii si spune cine a castigat:
+function the_winner(){
+    //aici schimb: in loc de getElemById las valoarea variabilei radio_checked pt player:
+    //let player = document.getElementById("user_pick").innerText;
     let radio_checked = document.querySelector('input[name="btnradio"]:checked').value;
-    document.getElementById("user_pick").innerHTML = radio_checked;
-    
-    //alegerea computerului, random din arrayul cu cele 3 variante:
+    let player = radio_checked;
+    //aici schimb: adaug alegerea computerului si evidentierea lui pe imagine direct, fara sa iau valoarea din elelmentul html computer_pick:
     const choices = ["rock","paper","scisor"];
     let computer_choice = choices[Math.floor(Math.random() * choices.length)];
-    document.getElementById("computer_pick").innerHTML = computer_choice;
+    let computer = computer_choice;
+    //let computer = document.getElementById("computer_pick").innerText;
     //alegerea computerului evidentiata pe imagine:
     if(computer_choice == "rock"){
         document.getElementById("comp_btnradio1").disabled = false;
@@ -27,81 +31,65 @@ function the_choices() {
         document.getElementById("comp_btnradio1").disabled = true;
         document.getElementById("comp_btnradio2").disabled = true;
     }
-    else{
-        
-    }
-}
 
-// foloseste functia switch !!
-//sunt variantele de combinatii si spune cine a castigat
-function the_winner(){
-    let player = document.getElementById("user_pick").innerText;
-    let computer = document.getElementById("computer_pick").innerText;
+
+let score;
     switch(true){
-    
+    //aici ar trebui sa schimb: in loc de .innerHTML ar trebui sa dau valoarea unei variabile let the_winner (am facut: e score variabila!!!), ca sa fie folosita de codul din functia urmatoare, care trebuie comasata cu functia asta(am facut), ca sa vada variabila the_winner !!!
         case player === "rock" && computer === "scisor":
-            document.getElementById("the_winner").innerHTML = "Player Win !!!";
+            score = "Player Win !!!";
             break;
             case player === "rock" && computer === "paper":
-            document.getElementById("the_winner").innerHTML = "Computer Win !!!";
+            score = "Computer Win !!!";
             break;
             case player === "paper" && computer === "rock":
-            document.getElementById("the_winner").innerHTML = "Player Win !!!";
+            score = "Player Win !!!";
             break;
             case player === "paper" && computer === "scisor":
-            document.getElementById("the_winner").innerHTML = "Computer Win !!!";
+            score = "Computer Win !!!";
             break;
             case player === "scisor" && computer === "rock":
-            document.getElementById("the_winner").innerHTML = "Computer Win !!!";
+            score = "Computer Win !!!";
             break;
             case player === "scisor" && computer === "paper":
-            document.getElementById("the_winner").innerHTML = "Player Win !!!";
+            score = "Player Win !!!";
             break;
     
             default:
-                document.getElementById("the_winner").innerHTML = "It's a tie.";
-    } 
-
+                score = "It's a tie.";
+    }
+    document.getElementById("the_round_winner").innerHTML = score;
 }
 
 
 // asta merge:
 // creaza un sir de 1 pt fiecare runda castigata
+
 function the_score(){
-    let score = document.getElementById("the_winner").innerText;
+    let calc_score = document.getElementById("the_round_winner").innerText;
+    let pl_sc_nr = Number(document.getElementById("pl_sc_nr").innerText);
+    let cp_sc_nr = Number(document.getElementById("cp_sc_nr").innerText);
+    let tie_sc_nr = Number(document.getElementById("tie_sc_nr").innerText);
     
     switch(true){
-        case score === "Player Win !!!":
-            document.getElementById("player_score").innerHTML = document.getElementById("player_score").innerHTML + 1;
+        case calc_score === "Player Win !!!":
+            pl_sc_nr = pl_sc_nr + 1;
+            document.getElementById("pl_sc_nr").innerHTML = pl_sc_nr;
             break;
-        case score === "Computer Win !!!":
-            document.getElementById("computer_score").innerHTML = document.getElementById("computer_score").innerHTML + 1;
+        case calc_score === "Computer Win !!!":
+            cp_sc_nr = cp_sc_nr + 1;
+            document.getElementById("cp_sc_nr").innerHTML = cp_sc_nr;
             break;
         default:
-            document.getElementById("tie_round").innerHTML = document.getElementById("tie_round").innerHTML + 1;
+            tie_sc_nr = tie_sc_nr + 1;
+            document.getElementById("tie_sc_nr").innerHTML = tie_sc_nr;
     }
 }
 
 
+//  !!!!!!!! de aici nu mai e legat codul de cel de sus si nu mai aduna sirul de 1111 ca sa dea scorul numeric !!!!
+// nu stiu ce am facut dar vad ca merge codul mai departe!!!
 
-//vede cati de 1 are in sir playerul si le transforma in numar (le numara)
-function pl_sc_nr(){
-    let player_score_total = document.getElementById("player_score").innerText;
-    let player_score_number = player_score_total.length;
-    document.getElementById("pl_sc_nr").innerHTML = player_score_number;
-}
-//vede cati de 1 are in sir computerul si le transforma in numar (le numara)
-function cp_sc_nr(){
-    let computer_score_total = document.getElementById("computer_score").innerText;
-    let computer_score_number = computer_score_total.length;
-    document.getElementById("cp_sc_nr").innerHTML = computer_score_number;
-}
-//vede cati de 1 are in sir tie score si le transforma in numar (le numara)
-function tie_sc_nr(){
-    let tie_score_total = document.getElementById("tie_round").innerText;
-    let tie_score_number = tie_score_total.length;
-    document.getElementById("tie_sc_nr").innerHTML = tie_score_number;
-}
 //acuma fac un meci sa dureze 5 runde, maxim!:
 //problema e la egalitatea or and, cred... ! Ba nu!!: e in variabile!!!!
 function end_game(){
@@ -110,26 +98,22 @@ function end_game(){
     let tie_sc_end = Number(document.getElementById("tie_sc_nr").innerText);
     let total_round = pl_sc_end + cp_sc_end + tie_sc_end;
     //linie de verificare:
-    document.getElementById("rounds_nr").innerHTML = total_round;
-    
-    //const btn = document.getElementById("myBtn");
+    document.getElementById("myCheck").innerHTML = total_round;
+    // face sa dispara butonul Play:
     if((pl_sc_end >= 3 || cp_sc_end >= 3) && total_round >= 5){
-        document.getElementById("play").style.visibility = "hidden";
+        document.getElementById("play").style.visibility = "hidden";   
     }
+    //scrie ca a castigat playerul:
     if(pl_sc_end >= 3 && total_round >= 5){
-        document.getElementById("the_game_winner").innerHTML = "Player Win !!!"
+        document.getElementById("the_game_winner").innerHTML = "PLAYER WIN !!!";
     }
+    //scrie ca a castigat computerul:
     if(cp_sc_end >= 3 && total_round >= 5){
-        document.getElementById("the_game_winner").innerHTML = "Computer Win !!!"
+        document.getElementById("the_game_winner").innerHTML = "COMPUTER WIN !!!";
     }
+
 }
-//numara rundele si activeaza adaugarea clasei disbla la buton, dupa 5 runde!!!
+//dupa fiecare click pe butonul Play verifica si activeaza functia end_game(), daca cineva indeplineste conditiile ei, adica castiga meciul!!!
 window.addEventListener("click", () => {
     end_game();
   });
-
-
-
-
-
-
